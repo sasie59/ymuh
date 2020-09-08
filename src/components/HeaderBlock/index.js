68daebdce0f443d1b7cf76a3fcb26d46751ad987
sasie59 <sasie59@gmail.com>
2020-09-08 08:25:22 +0800

    header區塊 雛形

--- src/components/HeaderBlock/index.js
+++ src/components/HeaderBlock/index.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import img from './logo.png';
 
 export default class HeaderBlock extends Component {
   render() {
     return (
       <div>
-        header
+        <div>
+          <img src={img} alt=""/>
+          <h2>國立陽明大學附設醫院</h2>
+        </div>
+        <div>
+          <input type="text"/>
+        </div>
       </div>
     )
   }
