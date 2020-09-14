a9c5c85fd777135cafd3b2fb56daefc084615993
sasie59 <sasie59@gmail.com>
2020-09-14 08:08:10 +0800

    reducers-內外婦兒 list

--- src/components/RealTimeBlock/index.js
+++ src/components/RealTimeBlock/index.js
@@ -1,41 +1,49 @@
-import React, { Component } from 'react'
-import Title from './components/TitleBlock';
-import Subject from './components/SubjectBlock';
+import React, { Component } from "react";
+import Title from "./components/TitleBlock";
+import Subject from "./components/SubjectBlock";
 
-import './style.scss';
+import "./style.scss";
 
 export default class RealTimeBlock extends Component {
   render() {
     return (
-      <div className='RealTimeBlock'>
-        <div className='title'>
-          <div style={{
-            backgroundColor: '#D8EBCA',
-            margin: '13px'
-          }}>
+      <div className="RealTimeBlock">
+        <div className="title">
+          <div
+            style={{
+              backgroundColor: "#D8EBCA",
+              margin: "13px",
+            }}
+          >
             <Title>謢理指導</Title>
+            <Subject />
           </div>
-          <div style={{
-            backgroundColor: '#CAEBDB',
-            margin: '13px'
-          }}>
+          <div
+            style={{
+              backgroundColor: "#CAEBDB",
+              margin: "13px",
+            }}
+          >
             <Title>衛教資訊</Title>
           </div>
-          <div style={{
-            backgroundColor: '#CADBEB',
-            margin: '13px'
-          }}>
+          <div
+            style={{
+              backgroundColor: "#CADBEB",
+              margin: "13px",
+            }}
+          >
             <Title>高齡友善</Title>
           </div>
-          <div style={{
-            backgroundColor: '#D3CAEB',
-            margin: '13px'
-          }}>
+          <div
+            style={{
+              backgroundColor: "#D3CAEB",
+              margin: "13px",
+            }}
+          >
             <Title>失智照顧</Title>
           </div>
-
         </div>
       </div>
-    )
+    );
   }
 }
